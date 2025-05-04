a23b45d82c924ae4e22a693f8ef2f7c77212c424
KimDoHyun <kdhqwe1030@naver.com>
2025-05-05 01:50:58 +0900

    Init: Route 환경구축

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import './style/font.css';
+import './styles/font.css';
 import { ThemeProvider } from 'styled-components';
-import theme from './style/theme';
+import { theme } from './styles/theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import GlobalStyle from './styles/globalStyle';
+import Router from './Router';
 
 const App = () => {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}></ThemeProvider>
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyle />
+        <Router />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 };
 
