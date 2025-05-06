b881310f9c091619f2d963f209d1519632e9c45b
KimDoHyun <kdhqwe1030@naver.com>
2025-05-07 00:02:02 +0900

    chore: 임의 theme데이터 disabledColor 색 변경

--- src/page/Home/data/theme.ts
+++ src/page/Home/data/theme.ts
@@ -2,21 +2,21 @@ export const brandtheme = {
   ediya: {
     mainColor: '#002F6C', // 이디야 메인 블루
     subColor: '#0051A3', // 이디야 서브 블루
-    disabledColor: '#BDBDBD', // 비활성화
+    disabledColor: 'rgba(0, 0, 0, 0.7)', // 비활성화
     textColor: '#212529',
     logoImg: 'https://i.imgur.com/ERWUmXR.png',
   },
   starbucks: {
     mainColor: '#00704A', // 스타벅스 그린
     subColor: '#1E3932', // 스타벅스 다크 그린
-    disabledColor: '#BDBDBD', // 비활성화
+    disabledColor: 'rgba(0, 0, 0, 0.7)', // 비활성화
     textColor: '#212529',
     logoImg: 'https://i.imgur.com/8aHKZoI.png',
   },
   twosomeplace: {
     mainColor: '#D5202C', // 투썸플레이스 레드
     subColor: '#8B0000', // 투썸플레이스 다크 레드
-    disabledColor: '#BDBDBD', // 비활성화
+    disabledColor: 'rgba(0, 0, 0, 0.7)', // 비활성화
     textColor: '#212529',
     logoImg: 'https://i.imgur.com/eSbE0ST.png',
   },
