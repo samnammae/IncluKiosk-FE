fa39f8fb8499da88b38c3fc275e7c2b4fb9297d8
KimDoHyun <kdhqwe1030@naver.com>
2025-05-08 18:27:53 +0900

    chore: 임의 tmp 데이터 textcolor변경

--- data/theme.ts
+++ data/theme.ts
@@ -3,7 +3,7 @@ export const brandtheme = {
     mainColor: '#002F6C', // 이디야 메인 블루
     subColor: '#0051A3', // 이디야 서브 블루
     disabledColor: 'rgba(0, 0, 0, 0.7)', // 비활성화
-    textColor: '#212529',
+    textColor: '#F8F9FA',
     logoImg: 'https://i.imgur.com/ERWUmXR.png',
     titleImg: 'https://i.imgur.com/ERWUmXR.png',
   },
@@ -11,7 +11,7 @@ export const brandtheme = {
     mainColor: '#00704A', // 스타벅스 그린
     subColor: '#1E3932', // 스타벅스 다크 그린
     disabledColor: 'rgba(0, 0, 0, 0.7)', // 비활성화
-    textColor: '#212529',
+    textColor: '#F8F9FA',
     logoImg: 'https://i.imgur.com/8aHKZoI.png',
     titleImg: 'https://i.imgur.com/8aHKZoI.png',
   },
@@ -19,7 +19,7 @@ export const brandtheme = {
     mainColor: '#D5202C', // 투썸플레이스 레드
     subColor: '#8B0000', // 투썸플레이스 다크 레드
     disabledColor: 'rgba(0, 0, 0, 0.7)', // 비활성화
-    textColor: '#212529',
+    textColor: '#F8F9FA',
     logoImg: 'https://i.imgur.com/eSbE0ST.png',
     titleImg: 'https://i.imgur.com/eSbE0ST.png',
   },
