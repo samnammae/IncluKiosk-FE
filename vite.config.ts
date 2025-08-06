96728ab8d39f8e00c32c874b44a8d95167546042
kdhqwe1030 <kdhqwe1030@naver.com>
2025-08-06 19:26:30 +0900

    fix: vite 빌드 시 상대 경로 문제 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [react()],
+    base: "/kiosk/",
     //빌드 관련 설정
     build: {
-      minify: 'terser',
+      minify: "terser",
       terserOptions: {
         compress: {
           drop_console: true,
@@ -18,7 +19,7 @@ export default defineConfig(({ mode }) => {
     server: {
       // 프록시 설정
       proxy: {
-        '/api': {
+        "/api": {
           target: env.VITE_SERVER_ADDRESS,
           changeOrigin: true,
         },
