5ed2395fa822b0f242168798112622ea3729ba96
kdhqwe1030 <kdhqwe1030@naver.com>
2025-08-22 15:22:32 +0900

    feat: 매장 조회 api 구현

---
+++ src/apis/shop.tsx
@@ -0,0 +1,9 @@
+import { api } from "./api";
+
+export const shopAPI = {
+  getAllShop: async () => {
+    const response = await api.get("/admin/store");
+    console.log("매장 전체 조회", response.data);
+    return response.data.data;
+  },
+};
