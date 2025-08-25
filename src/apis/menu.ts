28537d64221ef17fbed18974031d5a6dd8ff8ace
kdhqwe1030 <kdhqwe1030@naver.com>
2025-08-25 20:10:18 +0900

    feat: 메뉴 조회 api 구현

---
+++ src/apis/menu.ts
@@ -0,0 +1,14 @@
+import { api } from "./api";
+
+export const menuAPI = {
+  getCategory: async (storeId: number) => {
+    const response = await api.get(`/menu/${storeId}/category`);
+    console.log("카테고리 조회", response.data);
+    return response.data.data;
+  },
+  getAllMenu: async (storeId: number) => {
+    const response = await api.get(`/menu/${storeId}`);
+    console.log("메뉴 조회", response.data);
+    return response.data.data;
+  },
+};
