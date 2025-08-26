f297f8f69ded443aee338644e8b3fdbd7cf97481
kdhqwe1030 <kdhqwe1030@naver.com>
2025-08-26 16:38:17 +0900

    chore: 옵션 api return 수정

--- src/apis/menu.ts
+++ src/apis/menu.ts
@@ -14,6 +14,6 @@ export const menuAPI = {
   getAllOptions: async (storeId: number) => {
     const response = await api.get(`/menu/${storeId}/option`);
     console.log("옵션 전체 조회", response.data);
-    return response.data;
+    return response.data.data;
   },
 };
