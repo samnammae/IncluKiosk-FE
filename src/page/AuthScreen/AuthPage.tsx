9dfe1571979a59f0c97aa187f49e9b50abee4a33
kdhqwe1030 <kdhqwe1030@naver.com>
2025-09-26 13:42:55 +0900

    chore: 로그인이 되어 있는 경우 start페이지로 진입

--- src/page/AuthScreen/AuthPage.tsx
+++ src/page/AuthScreen/AuthPage.tsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Login from "./components/Login";
 import Join from "./components/Join";
 import IncluKiosk from "../../assets/imgs/IncluKiosk.png";
 import { Background, Title } from "./styles";
+import { useNavigate } from "react-router-dom";
+
 const AuthPage = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const nav = useNavigate();
   const changeMode = () => {
     setIsLogin(!isLogin);
   };
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) nav("/start");
+  }, []);
+
   return (
     <Background>
       <Title>
