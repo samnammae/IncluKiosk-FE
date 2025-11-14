c249dd052adfafec6a1190c2364285beee8129fd
kdhqwe1030 <kdhqwe1030@naver.com>
2025-11-14 21:44:43 +0900

    feat: 홈으로 돌아가기 기능추가

--- src/components/LockButton.tsx
+++ src/components/LockButton.tsx
@@ -1,30 +1,43 @@
 import styled from "styled-components";
 import { useLockStore } from "../stores/lockStore";
 import { useSocketStore } from "../stores/socketStore";
-
-const LockButton = () => {
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import { useNavigate } from "react-router-dom";
+const LockButton = ({ isHeader = false }: { isHeader?: boolean }) => {
   const { setLocked } = useLockStore();
   const { sendMessage } = useSocketStore();
+  const nav = useNavigate();
   return (
     <Container
       onClick={(e) => {
-        e.stopPropagation();
-        sendMessage({ type: "ALL_RESET" });
-        setLocked(true); //잠금화면으로 돌아가기
+        if (isHeader) {
+          nav("/start");
+        } else {
+          e.stopPropagation();
+          sendMessage({ type: "ALL_RESET" });
+          setLocked(true); //잠금화면으로 돌아가기
+        }
       }}
+      $isHeader={isHeader}
     >
-      잠금화면으로 돌아가기
+      {isHeader ? <HomeOutlinedIcon /> : <></>}
+
+      {isHeader ? " 홈화면" : "잠금화면으로 돌아가기"}
     </Container>
   );
 };
 
 export default LockButton;
 
-const Container = styled.button`
+const Container = styled.button<{ $isHeader: boolean }>`
   position: absolute;
   top: 4rem;
   left: 2rem;
   z-index: 10;
+
+  display: flex;
+  align-items: center;
+  gap: 10px;
 
   padding: 0.8rem 1.2rem;
   border-radius: ${({ theme }) => theme.borderRadius.lg};
@@ -35,9 +48,11 @@ const Container = styled.button`
 
   border: 1px solid rgba(255, 255, 255, 0.4);
 
-  color: ${({ theme }) => theme.colors.white};
+  color: ${({ theme, $isHeader }) =>
+    $isHeader ? theme.colors.main : theme.colors.white};
   font-size: ${({ theme }) => theme.fonts.sizes.xs};
-  font-weight: ${({ theme }) => theme.fonts.weights.medium};
+  font-weight: ${({ theme, $isHeader }) =>
+    $isHeader ? theme.fonts.weights.bold : theme.fonts.weights.medium};
 
   cursor: pointer;
   transition: 0.25s ease;
